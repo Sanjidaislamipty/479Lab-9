568eb2daf4cd6004867b8a259bb86ccc27484d31
Sanjidaislamipty <149031459+Sanjidaislamipty@users.noreply.github.com>
2024-05-17 22:00:45 +0600

    Add files via upload
    New File Uploaded

---
+++ cv.php
@@ -0,0 +1,219 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>2020-2-60-156</title>
+</head>
+<body>
+<table width="1300">
+<tr>
+<td><h2> Sanjida Islam Ipty</h2></td>
+<td align ="right"; rowspan="4"><img src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRbzkyXfNfGvQcUB3Kh0PrNLmT9LvbiJGglqAYDd6DD&s" height="180"width="90"/></td>
+</tr>
+<tr>
+<td>Dhaka,Bangladesh</td>
+</tr>
+<tr>
+<td>Email: islamsanjida17@gmail.com</td>
+</tr>
+<tr>
+<td>Tel: 01616769861</td>
+</tr>
+</table>
+<br>
+<h3> Web Database Programming</h3><hr/><br/>
+<p>Focused and diligent graduate in data sciences looks to leverage in-depth knowledge of data analysis to drive success in the business intelligence team at "XYZ". Participated in research projects while at university. Proficient in Python, and Excel.</p><br/>
+<h3> Present Status</h3><hr/><br/>
+<p>Undergraduate student at East West University</p><br/>
+<h3> Education</h3><hr/><br/>
+<table width="1300"; border="">
+<tr>
+<td><h5> Exam</h5></td>
+<td><h5> Board/University</h5></td>
+<td><h5> Year</h5></td>
+<td><h5> Name of Institution</h5></td>
+<td><h5> Marks</h5></td>
+</tr>
+<tr>
+<td>SSC </td>
+<td>"" </td>
+<td>"" </td>
+<td>""</td>
+<td> ""</td>
+</tr>
+</tr>
+<tr>
+<td>HSC </td>
+<td> ""</td>
+<td>"" </td>
+<td>"" </td>
+<td> ""</td>
+</tr>
+</tr>
+</table>
+<br>
+<h3> Computer Skills</h3><hr/><br/>
+<table>
+<tr>
+<td> Programming Languages</td>
+<td>: </td>
+<td>C, C++, Java, Php, Python </td>
+</tr>
+<tr>
+<td> Web based skills</td>
+<td>: </td>
+<td>HTML, CSS, PHP </td>
+</tr>
+<tr>
+<td> Database skills</td>
+<td>: </td>
+<td>SQL </td>
+</tr>
+<tr>
+<td>Scripting skills</td>
+<td>: </td>
+<td> </td>
+</tr>
+<tr>
+<td>Development Tools </td>
+<td>: </td>
+<td>VS code,Git-hub </td>
+</tr>
+<tr>
+<td> Others</td>
+<td>: </td>
+<td> </td>
+</tr>
+</table>
+<h3> Professional Experience</h3><hr/><br/>
+<p>No experience</p>
+<h3> Projects</h3><hr/><br/>
+<p>Do not work in any professional project.</p>
+<h3>Academic Projects</h3><hr/><br/>
+<ul>
+<li>TIC TAC TOE
+<ul>
+<li>Generally the game board size is 3x3 but I developed the game where users can input the board size in n*n form.</li>
+</ul>
+</li>
+</ul>
+<ul>
+    <li>PORTAL MANAGEMENT SYSTEM
+    <ul>
+    <li>I with my team developed a system that would help a portal admin to manage his system, student information, and subject information.</li>
+    </ul>
+    </li>
+    </ul>
+    <ul>
+        <li>DENTAL CLINIC PROBLEM
+        <ul>
+        <li>  We had to synchronize the activities of a dental clinic using threads, semaphores and MUTEX locks.</li>
+        </ul>
+        </li>
+        </ul>
+        <ul>
+            <li>N QUEEN
+            <ul>
+            <li>Generally the game board size is n x n but I developed the game where users can input the board size in n*n form.</li>
+            </ul>
+            </li>
+            </ul>
+            <ul>
+                <li>SHOP MANAGEMENT SYSTEM
+                <ul>
+                <li>My team developed a system to manage price, stock, quantity, customers and print payment slip.</li>
+                </ul>
+                </li>
+                </ul>
+                <ul>
+<h3> Achievments</h3><hr/><br/>
+<ul>
+<li> No achievments
+</li>
+</ul>
+<h3> Language</h3><hr/><br/>
+<ul>
+    <li> Bangla
+    </li>
+    </ul>
+    <ul>
+        <li> English
+        </li>
+        </ul>
+<h3> Personal Information</h3><hr/><br/>
+<table align="center">
+<tr>
+<td>Full Name </td>
+<td>: </td>
+<td>"" </td>
+</tr>
+<tr>
+<td> Father's Name</td>
+<td>: </td>
+<td>"Safiqul Islam" </td>
+</tr>
+<tr>
+<td>Mother's Name </td>
+<td>: </td>
+<td>"Nurjahan Akter " </td>
+</tr>
+<tr>
+<td>Date of Birth</td>
+<td>: </td>
+<td>""</td>
+</tr>
+
+        <tr>
+            <td>Sex</td>
+            <td>: </td>
+            <td>Female </td>
+            </tr>
+            <tr>
+                <td>Marital Status</td>
+                <td>: </td>
+                <td>Single </td>
+                </tr>
+                <tr>
+                    <td>Nationality</td>
+                    <td>: </td>
+                    <td> Bangladesh</td>
+                    </tr>
+                    <tr>
+                        <td>Religion</td>
+                        <td>: </td>
+                        <td>   Islam</td>
+                        </tr>
+                        <tr>
+                            <td>Blood Group</td>
+                            <td>: </td>
+                            <td> A+</td>
+                            </tr>
+                            <tr>
+                                <td>Present Address</td>
+                                <td>: </td>
+                                <td>Konapara, Demra,Dhaka </td>
+                                </tr>
+                                <tr>
+                                    <td>Permanent Address</td>
+                                    <td>: </td>
+                                    <td> Konapara, Demra,Dhaka </td>
+                                    </tr>
+</table>
+<br>
+<h3> References</h3><hr/><br/>
+<h5>Dr. Md. Rezaul Karim </h5><br/>
+<p>
+
+    Professor, Department of Computer Science and Engineering<br/>
+   
+    University of EWU<br/>
+   
+    Email:korim.kaiser@ewubd.com</p><br/>
+<h5> Mahamudul Hasan</h5><br/>
+<p> Department of Computer Science and Engineering<br/>
+   
+    East West University<br/>
+   
+    Email: Mahamudulhasan.cse@ewubd.edu </p><br/>
+<p align = "center">Copyright ©️ Sanjida Islam Ipty, 2016</p>
+</body>
+</html>
